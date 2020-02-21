47131ba570f403e12107c5ff280399c55e991b46
Евгений Шныркевич <mogrein.dark@gmail.com>
2020-02-21 13:14:27 +0300

    feaat: realize typical arrays problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,49 @@
 
 exports.min = function min (array) {
-  return 0;
+
+  if(!Array.isArray(array)){
+    return 0;
+  } else if(array.length == 0){
+    return 0;
+  }
+
+  let minimum = array[0];
+  for(let i = 0; i < array.length; i++){
+    if(minimum > array[i]){
+      minimum = array[i];
+    }
+  }
+  return minimum;
 }
 
 exports.max = function max (array) {
-  return 0;
+
+  if(!Array.isArray(array)){
+    return 0;
+  } else if(array.length == 0){
+    return 0;
+  }
+
+  let maximum = array[0];
+  for(let i = 0; i < array.length; i++){
+    if(maximum < array[i]){
+      maximum = array[i];
+    }
+  }
+  return maximum;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+
+  if(!Array.isArray(array)){
+    return 0;
+  } else if(array.length == 0){
+    return 0;
+  }
+
+  let average = 0;
+  for(let i = 0; i < array.length; i++){
+    average += array[i];
+  }
+  return average/array.length;
 }
